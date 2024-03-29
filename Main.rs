e7540bd221e854fc96cd4cae4c5ef4c8b1191e1c
natsugiri <natsugiri@gmail.com>
2024-03-30 01:09:17 +0900

    [Main] read tuple

--- Main.rs
+++ Main.rs
@@ -1,14 +1,36 @@
-fn read<T> () -> T where T: std::str::FromStr {
-    let mut line = String::new();
-    std::io::stdin().read_line(&mut line).ok();
-    line.trim().parse().ok().unwrap()
-}
+mod input {
+    use std::str::FromStr;
 
-fn read_vec<T> () -> Vec<T> where T: std::str::FromStr {
-    let mut line = String::new();
-    std::io::stdin().read_line(&mut line).ok();
-    line.split_whitespace().map(|t| t.parse().ok().unwrap()).collect()
+    pub fn read_line() -> String {
+	let mut line = String::new();
+	std::io::stdin().read_line(&mut line).ok();
+	line.trim().into()
+    }
+
+    pub fn read<T: FromStr>() -> T {
+	read_line().parse().ok().unwrap()
+    }
+
+    pub fn read_vec<T: FromStr>() -> Vec<T> {
+	read_line().split_whitespace().map(|t| t.parse().ok().unwrap()).collect()
+    }
+
+    pub fn read2<T: FromStr, U: FromStr>() -> (T, U) {
+	let line = read_line();
+	let mut a = line.split_ascii_whitespace();
+	(a.next().unwrap().parse().ok().unwrap(),
+	a.next().unwrap().parse().ok().unwrap())
+    }
+
+    pub fn read3<T: FromStr, U: FromStr, V: FromStr>() -> (T, U, V) {
+	let line = read_line();
+	let mut a = line.split_ascii_whitespace();
+	(a.next().unwrap().parse().ok().unwrap(),
+	a.next().unwrap().parse().ok().unwrap(),
+	a.next().unwrap().parse().ok().unwrap())
+    }
 }
+use input::*;
 
 fn main() {
     let n = read::<usize>();
