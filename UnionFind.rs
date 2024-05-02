78a8f6d66030c56cce39d68abdb7d9e244fc22c2
natsugiri <natsugiri@gmail.com>
2024-05-02 17:52:55 +0900

    [UnionFind] add count_node, count_tree

--- UnionFind.rs
+++ UnionFind.rs
@@ -1,37 +1,48 @@
-struct UnionFind {
-    a: Vec<i32>,
-}
-
-impl UnionFind {
-    fn new(n: usize) -> UnionFind {
-        UnionFind{ a: vec![-1; n] }
+mod union_find {
+    pub struct UnionFind {
+        cc: usize,
+        a: Vec<isize>,
     }
 
-    fn root(&mut self, x: usize) -> usize {
-        if self.a[x] < 0 {
-            x
-        } else {
-            let r = self.root(self.a[x] as usize);
-            self.a[x] = r as i32;
-            r
+    impl UnionFind {
+        pub fn new(n: usize) -> UnionFind {
+            UnionFind{ cc: n, a: vec![-1; n] }
         }
-    }
 
-    fn link(&mut self, x: usize, y: usize) {
-        let mut x = self.root(x);
-        let mut y = self.root(y);
-        if x == y {
-            ()
-        } else {
-            if self.a[x] > self.a[y] {
-                std::mem::swap(&mut x, &mut y);
+        pub fn root(&mut self, x: usize) -> usize {
+            if self.a[x] < 0 {
+                x
+            } else {
+                let r = self.root(self.a[x] as usize);
+                self.a[x] = r as isize;
+                r
             }
-            self.a[x] += self.a[y];
-            self.a[y] = x as i32;
+        }
+
+        pub fn link(&mut self, x: usize, y: usize) {
+            let mut x = self.root(x);
+            let mut y = self.root(y);
+            if x != y {
+                if self.a[x] > self.a[y] {
+                    std::mem::swap(&mut x, &mut y);
+                }
+                self.a[x] += self.a[y];
+                self.a[y] = x as isize;
+                self.cc -= 1;
+            }
+        }
+
+        pub fn same(&mut self, x: usize, y: usize) -> bool {
+            self.root(x) == self.root(y)
+        }
+
+        pub fn count_node(&mut self, x: usize) -> usize {
+            let x = self.root(x);
+            (-self.a[x]) as usize
+        }
+
+        pub fn count_tree(&self) -> usize {
+            self.cc
         }
     }
-
-    fn same(&mut self, x: usize, y: usize) -> bool {
-        self.root(x) == self.root(y)
-    }
-}
+} // mod union_find;
